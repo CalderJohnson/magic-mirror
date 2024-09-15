520c4627dbd7b09fc1c93f5d6fb99f0642bb77bc
Calder Johnson <calderbjohnson@icloud.com>
2024-09-14 22:21:38 -0400

    Updated requirements

--- api/api.py
+++ api/api.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
